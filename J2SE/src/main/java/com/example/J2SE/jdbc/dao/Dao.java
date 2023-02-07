9d59e2f63e3a1b9baa53c169304cc8e18752a184
unknown <rajkabee@gmail.com>
2023-02-07 11:28:09 +0545

    main

---
+++ J2SE/src/main/java/com/example/J2SE/jdbc/dao/Dao.java
@@ -0,0 +1,15 @@
+package com.example.J2SE.jdbc.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Dao<T> {
+	public void connect() throws ClassNotFoundException, SQLException;
+	public void disconnect() throws ClassNotFoundException, SQLException;
+	public int save(T t) throws ClassNotFoundException, SQLException;
+	public T getOne(int id) throws ClassNotFoundException, SQLException;
+	public List<T> getAll() throws ClassNotFoundException, SQLException;
+	public int update(T t) throws ClassNotFoundException, SQLException;
+	public int delete(int id) throws ClassNotFoundException, SQLException;
+	
+}
